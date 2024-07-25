bd6bd48e8cf3318c549514076ee34027194ddd40
Arseny Koshelnik <arskosh05@mail.ru>
2024-07-26 01:51:00 +0700

    Initial

---
+++ src/main/java/com/arsenydeveloper/Main.java
@@ -0,0 +1,20 @@
+package com.arsenydeveloper;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.ArrayList;
+
+/**
+ * Main
+ */
+public class Main {
+
+    public static void main(String[] args) {
+    }
+
+    public static char findByStream(String input) {
+        input.chars()
+            .forEach(System.out::println);
+        return 'c';
+    }
+}
---
+++ src/main/java/com/arsenydeveloper/lection/First.java
@@ -0,0 +1,9 @@
+package com.arsenydeveloper.lection;
+
+/**
+ * First
+ */
+public class First {
+
+    public static void main(String[] args) {}
+}
